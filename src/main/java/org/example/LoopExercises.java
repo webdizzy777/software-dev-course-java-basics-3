d4af55705bb31f057de03097b71f83537adc9436
webdizzy777 <carissa.mcmillian@gmail.com>
2025-08-31 20:30:49 -0500

    updated loops

--- src/main/java/org/example/LoopExercises.java
+++ src/main/java/org/example/LoopExercises.java
@@ -4,11 +4,11 @@ public class LoopExercises {
     public int sum(int n) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
         // (use a for loop)
-        int total = 1;
-        for (int i = 0; i < n; i++){
-            total += n;
+        int total = 0;
+        for (int i = 0; i <= n; i++){
+            total += i;
         }
-        return  n;
+        return  total;
     }
 
     public int sumUntilEven(int n) {
@@ -16,14 +16,14 @@ public class LoopExercises {
         // but stops adding when the sum is even
         // (use a while loop with a sum variable and a counter variable)
         int sum = 0;
-        int counter = 0;
+        int counter = 1;
         while (counter <= n){
-            sum +=n;
+            sum += counter;
             if (sum % 2 == 0){
                 break;
             }
             counter ++;
         }
-        return n;
+        return sum;
     }
 }
